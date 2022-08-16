6c1267dab7ab50d23b6ce3aadb345dcf6f51662f
tuguscript <tuguscript@gmail.com>
2022-08-15 17:03:42 -0700

    :zap:

--- pages/addSuppliers.tsx
+++ pages/addSuppliers.tsx
@@ -1,26 +1,22 @@
 import AddSupplierForm from "components/AddSupplierForm";
 import Layout from "components/Layouts";
-import React, { Component } from "react";
 import Head from "next/head";
+import React from "react";
 
 type Props = {};
 
-type State = {};
-
-export default class addSuppliers extends Component<Props, State> {
-  state = {};
-
-  render() {
-    return (
-      <div className="dark:bg-gray-800">
-        <Head>
-          <title>Create Next App</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Layout>
-          <AddSupplierForm />
-        </Layout>
-      </div>
-    );
-  }
+function addSuppliers({}: Props) {
+  return (
+    <div className="dark:bg-gray-800">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <AddSupplierForm />
+      </Layout>
+    </div>
+  );
 }
+
+export default addSuppliers;
